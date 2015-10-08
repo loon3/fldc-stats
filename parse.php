25846b26383dfa48b05b6e7c0aa8d315e07d8d1f
Joe Looney <hello@joelooney.org>
2015-10-08 07:43:26 -0400

    formatting

--- parse.php
+++ parse.php
@@ -10,8 +10,8 @@ $mysqli = new mysqli($dbserver, $dbuser, $dbpassword, $dbname);
 
 /* check connection */
 if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+    	printf("Connect failed: %s\n", mysqli_connect_error());
+    	exit();
 }
 
 //create new table template for daily stats
@@ -27,39 +27,40 @@ if ($mysqli->query($tablecreate) === TRUE) {
 	$filename = $day.".txt";	    
 	$i = 0;
        
-    //parse data from [TODAY].txt line-by-line
+    	//parse data from [TODAY].txt line-by-line
 	$handle = @fopen($filename, "r");
 	if ($handle) {
-    	while (($buffer = fgets($handle, 4096)) !== false) {
+    	
+    		while (($buffer = fgets($handle, 4096)) !== false) {
              
-        	//skip first two lines of data file
-            if($i > 1) {
+	        	//skip first two lines of data file
+	            	if($i > 1) {
 
-        		$linedata = explode("	", $buffer);
-        		$name = $linedata[0];
+	        		$linedata = explode("	", $buffer);
+	        		$name = $linedata[0];
 				$credit = $linedata[1];
 				
-                //usernames must be at least 26 characters
+        		       	//usernames must be at least 26 characters
 				if (strlen($name) >= 26) {
                     
-				    //check if member of Foldingcoin team (226728) and using bitcoin address only username                
+					//check if member of Foldingcoin team (226728) and using bitcoin address only username                
 					$team = trim($linedata[3]);
 				
 					if ($team == "226728") {
 					
 						$isvalid = checkAddress($name);
         						
-        				if($isvalid == 1) {
+        					if($isvalid == 1) {
         			
-        					$namelinedata[0] = "---";
-        					$namelinedata[1] = "FLDC";
-        					$namelinedata[2] = $name;
+        						$namelinedata[0] = "---";
+        						$namelinedata[1] = "FLDC";
+        						$namelinedata[2] = $name;
         				
-        				} else {
+        					} else {
         				
-        					$namelinedata = explode("_", $buffer);
+        						$namelinedata = explode("_", $buffer);
         				
-        				}
+        					}
 					
 					} else {
 				
@@ -67,40 +68,41 @@ if ($mysqli->query($tablecreate) === TRUE) {
 					
 					}
                     
-                    //check for Foldingcoin user by naming convention	
+                    			//check for Foldingcoin user by naming convention	
 					if (count($namelinedata) == 3 || $namelinedata[0] == "---") {
                         
-                        //check if token indicated in username is valid
+                        			//check if token indicated in username is valid
 						if($namelinedata[1] == "ALL" || $namelinedata[1] == "FLDC" || $namelinedata[1] == "OCTO" || $namelinedata[1] == "MAGICFLDC" || $namelinedata[1] == "SCOTCOIN"){
 					
-        					$fldcname = $namelinedata[0];
-        					$token = $namelinedata[1];
+        						$fldcname = $namelinedata[0];
+        						$token = $namelinedata[1];
         				
-        					$addressdata = explode("	",$namelinedata[2]);
+        						$addressdata = explode("	",$namelinedata[2]);
         				
-        					$address = $addressdata[0];
+        						$address = $addressdata[0];
         				    
-                            //check if bitcoin address is valid       
-        					if(strlen($address) >= 26 && strlen($address) <= 35 && substr($address,0,1) == 1 ) {
-        						if (ctype_alnum($address)) {
+                            				//check if bitcoin address is valid       
+        						if(strlen($address) >= 26 && strlen($address) <= 35 && substr($address,0,1) == 1 ) {
         						
-        							$isvalid = checkAddress($address);
+        							if (ctype_alnum($address)) {
         						
-        							if($isvalid == 1) {
+        								$isvalid = checkAddress($address);
+        						
+        								if($isvalid == 1) {
         							
-        								echo $name." - ";
-        								echo $credit."<br>";
+        									echo $name." - ";
+        									echo $credit."<br>";
                                         
-                                        //enter foldingcoin user information into database
-        								$tableentry = "INSERT INTO `$dbname`.`$day` (`name`, `token`, `address`, `totalpts`) VALUES ('$fldcname','$token','$address','$credit')";
+                                        					//enter foldingcoin user information into database
+        									$tableentry = "INSERT INTO `$dbname`.`$day` (`name`, `token`, `address`, `totalpts`) VALUES ('$fldcname','$token','$address','$credit')";
 
 										$mysqli->query($tableentry);
 								
-        							}
+        								}
                                     
-        						}
+        							}
 						
-        					}
+        						}
         			
 						}
                         
@@ -108,16 +110,16 @@ if ($mysqli->query($tablecreate) === TRUE) {
                     
 				}
                 
-        	}
+        		}
 		
-			//LIMIT FOR TESTING ONLY
+		//LIMIT FOR TESTING ONLY
         	//if($i > 10000) {break;}
         
         	$i++;
         
-    	}
+    		}
     	
-    	fclose($handle);
+    		fclose($handle);
     	
 	}
 
